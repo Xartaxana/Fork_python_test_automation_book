f340278655f6af79e295b83858b49a529b3c67eb
Mariia_Lobashova <mariia_lobashova@epam.com>
2025-01-26 15:51:27 +0100

    Homework 05 API testing

--- topics/api_testing/tests/test_api.py
+++ topics/api_testing/tests/test_api.py
@@ -3,14 +3,13 @@ import requests
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 from unittest.mock import patch
-from topics.Project.Core.logger import get_logger
+from ..core.logger import get_logger
 
 # Initialize the logger
 logger = get_logger(__name__)
 logger.info(" API logger initialized successfully!")
 logger.info("This is an info message.")
 logger.debug("This is a debug message.")
-
 
 base_url = "https://jsonplaceholder.typicode.com/"
 
@@ -20,9 +19,9 @@ def headers():
     return {"Content-Type": "application/json"}
 
 
-# Test GET request
 @pytest.mark.api
 def test_get_request(headers):
+    """Test to validate that the GET method fetches data as expected."""
     logger.debug("Starting log for test_get_request")
     response = requests.get(f"{base_url}/posts/1", headers=headers)
     assert response.status_code == 200
@@ -42,22 +41,23 @@ user_schema = {
 }
 
 
-# Test POST request with JSON data
+@pytest.mark.parametrize("body", ["1", "2", "3", "4", "5"])
 @pytest.mark.api
-def test_post_request(headers):
+def test_post_request(headers, body):
+    """Test to validate that the POST method creates data according to schema."""
     logger.debug("Starting log for test_post_request")
-    payload = {"body": "bar", "userId": 1}
+    payload = {"body": body, "userId": 1}
     response = requests.post(f"{base_url}/posts", json=payload, headers=headers)
     assert response.status_code == 201
-    assert response.json()["body"] == "bar"
+    assert response.json()["body"] == body
     post_data = response.json()
     validate(instance=post_data, schema=user_schema)
     logger.debug("test_post_request successfully passed")
 
 
-# Test POST  with error
 @pytest.mark.api
 def test_post_with_validation_error(headers):
+    """Test to validate that schema validation work as expected."""
     logger.debug("Starting log for test_post_with_validation_error")
     payload = {"title": "lalala", "userId": 2}
     response = requests.post(f"{base_url}/posts", json=payload, headers=headers)
@@ -71,16 +71,38 @@ def test_post_with_validation_error(headers):
         print(e)
 
 
+@patch("requests.get")
+def test_connection_error(mock_get, headers):
+    """GET request: simulating a connection error"""
+    logger.debug("Starting log for test_connection_error")
+    mock_get.side_effect = requests.exceptions.ConnectionError("Connection error")
+    try:
+        response = requests.get(f"{base_url}/posts/1", headers=headers)
+        assert response.status_code == 200
+        logger.debug("test_connection_error successfully passed")
+    except requests.exceptions.ConnectionError as e:
+        logger.error("The request caused an error")
+        print(f"Caught network error: {e}")
+
+
 @pytest.mark.api
 @patch('requests.get')
 def test_mocked_response_in_put_request(mock_get, headers):
+    """Test to validate that the PUT method update data with new data from mocks."""
     logger.debug("Starting log for test_mocked_response_in_put_request")
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = {"title": "Mocked Title", "body": "Mocked body", "userId": 3}
     mocked_response = requests.get(f"{base_url}/posts/1")
-    put_response = requests.put(f"{base_url}/posts/1", json=mocked_response.json(), headers=headers)
     # PUT mocked data in real DB
+    put_response = requests.put(f"{base_url}/posts/1", json=mocked_response.json(), headers=headers)
     assert put_response.status_code == 200
     assert put_response.json()["title"] == "Mocked Title"
     assert put_response.json()["userId"] == 3
     logger.debug("test_mocked_response_in_put_request successfully passed")
+
+
+@pytest.mark.api
+def test_delete_request():
+    """Test to validate delete method."""
+    response = requests.delete(f"{base_url}/posts/1")
+    assert response.status_code == 200
